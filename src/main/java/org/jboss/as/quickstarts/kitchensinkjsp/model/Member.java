51ddd18178dacf3ad78cbf2ff0ac2ca6f93a7ea7
ulxdemo1 <demo1@ulx.hu>
2020-03-18 12:29:48 +0000

    Commit Message

--- src/main/java/org/jboss/as/quickstarts/kitchensinkjsp/model/Member.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensinkjsp/model/Member.java
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * Modifications copyright 2019 Red Hat, Inc.
  */
 package org.jboss.as.quickstarts.kitchensinkjsp.model;
@@ -93,10 +93,10 @@ public class Member implements Serializable {
    public void setPhoneNumber(String phoneNumber) {
       this.phoneNumber = phoneNumber;
    }
-   
-   
+
+
    public String toString(){
 	return getName()+":"+getEmail()+":"+getPhoneNumber();
-	   
+
    }
 }
